9b68aff278fc38015aa4ced57e21059a76cf5ac2
Amine Mouafik <amine@mouafik.fr>
2015-04-03 12:36:05 +0700

    config variable passable through env variable

--- conf.js
+++ conf.js
@@ -1,4 +1,4 @@
 module.exports = {
-  appId: '4fb6755904e14188a0098e2eb710efb1',
-  protocol: 'http'
+  appId: process.env.OXR_APP_ID || '4fb6755904e14188a0098e2eb710efb1',
+  protocol: process.env.OXR_PROTOCOL || 'https'
 }
